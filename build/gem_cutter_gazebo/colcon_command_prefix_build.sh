b6ddb0566cf459f5ec07a2312a9e10ea7d1ce4b5
Nihara Randini <rdniharash@gmail.com>
2025-12-24 19:02:57 +0530

    Add files via upload

---
+++ build/gem_cutter_gazebo/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/kiran_gunathilaka/development/ros/gem_cutter_manipulator/install/gem_cutter_description/share/gem_cutter_description/package.sh"
